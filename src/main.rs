62035ed4d881ba6fc24e95568bbfb90170b21767
BKSalman <kfoo2023@gmail.com>
2022-09-21 22:40:40 +0300

    changed events crate from enigo to rdev for better compatibility + different way of communication for events

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 // #![windows_subsystem = "windows"]
-use std::fs;
-use bevy::reflect::erased_serde::private::serde::Deserialize;
-use bevy::{prelude::*, window::{PresentMode, WindowId}, render::{camera::WindowOrigin, render_resource::FilterMode}, winit::WinitWindows};
-use enigo::Enigo;
+use bevy::{
+    prelude::*,
+    render::{camera::WindowOrigin, render_resource::FilterMode},
+    window::{PresentMode, WindowId},
+    winit::WinitWindows,
+};
+use crossbeam_channel::{unbounded, Receiver};
+use std::{fs, thread};
 // use debug::DebugPlugin;
 // use bevy_inspector_egui::prelude::*;
-use winit::window::Icon;
+use rdev::listen;
 use ron::de::from_str;
+use serde::Deserialize;
+use winit::window::Icon;
 
 // mod debug;
 
@@ -18,12 +24,18 @@ struct Config {
     x_offset: f32,
     y_offset: f32,
     size: Vec2,
-    background: String
+    background: String,
 }
+
+#[derive(Deref)]
+pub struct StreamReceiver(Receiver<rdev::Event>);
+
+#[derive(Debug)]
+struct StreamEvent(rdev::Event);
 
 #[derive(Default, Component, Debug)]
 pub struct Hand {
-    config: Config
+    config: Config,
 }
 
 fn main() {
@@ -35,42 +47,39 @@ fn main() {
     });
 
     App::new()
-    .insert_resource(WindowDescriptor {
-        height: HEIGHT,
-        width: HEIGHT * RESOLUTION,
-        position: WindowPosition::Centered(MonitorSelection::Primary), // optional
-        title: "Tablet tracker".into(), // optional
-        present_mode: PresentMode::Fifo,
-        ..Default::default()
-    })
-    .insert_resource(FilterMode::Nearest)
-    .insert_resource(config)
-    .insert_resource(ClearColor)
-    .add_plugins(DefaultPlugins)
-    
-    // plugin for debugging the entities and components using "bevy-inspector-egui"
-    // .add_plugin(DebugPlugin) 
-    .add_startup_system(set_window_icon)
-    .add_startup_system(spawn_camera)
-    .add_startup_system(load_image)
-    .add_startup_system(image_size)
-    .add_system(background_color)
-    .add_system(image_movement)
-    .run();
+        .insert_resource(WindowDescriptor {
+            height: HEIGHT,
+            width: HEIGHT * RESOLUTION,
+            position: WindowPosition::Centered(MonitorSelection::Primary), // optional
+            title: "Tablet tracker".into(),                                // optional
+            present_mode: PresentMode::Fifo,
+            ..Default::default()
+        })
+        .add_event::<StreamEvent>()
+        .insert_resource(FilterMode::Nearest)
+        .insert_resource(config)
+        .insert_resource(ClearColor)
+        .add_plugins(DefaultPlugins)
+        // plugin for debugging the entities and components using "bevy-inspector-egui"
+        // .add_plugin(DebugPlugin)
+        .add_startup_system(set_window_icon)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(load_image)
+        .add_startup_system(image_size)
+        .add_startup_system(setup_events)
+        .add_system(read_stream)
+        .add_system(background_color)
+        .add_system(image_movement)
+        .run();
 }
 
-fn spawn_camera(
-    mut commands: Commands,
-) {
+fn spawn_camera(mut commands: Commands) {
     let mut camera = Camera2dBundle::new_with_far(1.);
     camera.projection.window_origin = WindowOrigin::BottomLeft;
     commands.spawn_bundle(camera);
 }
 
-fn background_color(
-    mut bg_color: ResMut<ClearColor>,
-    config: Res<Config>
-) {
+fn background_color(mut bg_color: ResMut<ClearColor>, config: Res<Config>) {
     let bg = config.background.clone();
     if bg.to_ascii_lowercase() == "green".to_string() {
         bg_color.0 = Color::GREEN;
@@ -79,92 +88,7 @@ fn background_color(
     }
 }
 
-fn load_image(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    config: Res<Config>
-) {
-    let texture: Handle<Image> = asset_server.load("Hand.png");
-
-    let sprite = Sprite{
-        anchor: bevy::sprite::Anchor::TopLeft,
-        ..Default::default()
-    };
-
-    commands.spawn_bundle(SpriteBundle{
-        sprite,
-        texture,
-        ..Default::default()
-    })
-    .insert(Name::new("Hand"))
-    .insert(Transform::from_xyz(0., 0., 0.))
-    .insert(Hand {
-        config: Config { 
-            x_offset: config.x_offset,
-            y_offset: config.y_offset,
-            size: config.size,
-            ..Default::default()
-         }
-    });
-}
-
-fn image_size(
-    mut hand_query: Query<(&Hand, &mut Sprite)>,
-) {
-    if let Ok(mut hand) = hand_query.get_single_mut() {
-        if hand.0.config.size.x > 0. && hand.0.config.size.y > 0. {
-            hand.1.custom_size = Some(hand.0.config.size);
-        }
-    } else {
-
-    }
-}
-
-fn image_movement(
-    windows: Res<Windows>,
-    mut hand_query: Query<(&Hand, &mut Transform)>,
-) {
-    // get the window resource from bevy
-    let window = windows.get_primary();
-    // get the mouse location in a tuple
-    // mouse.0 = x mouse.1 = y
-    let mouse: (i32, i32) = Enigo::mouse_location();
-    
-    // get the image
-    // hand.0 = Hand, hand.1 Transform
-    if let Ok(mut hand) = hand_query.get_single_mut() {
-        match window {
-            Some(window) => {
-                // check if available
-                // then get the position of the window
-                if let Some(position) = window.position() {
-                    // subtract the position of the window from the mouse location
-                    // to get the relative location
-            
-                    let x = (mouse.0 - position.x) as f32 + hand.0.config.x_offset; // the more offset the more the images goes to the right
-                    let y = (mouse.1 - position.y) as f32 - hand.0.config.y_offset; // the more offset the more the images goes up
-                    
-                    // rotating the image to add hand like effect
-                    hand.1.rotation = Quat::from_rotation_z( 
-                        ((y * 0.8) / window.height()) - (((mouse.0 - position.x) as f32 - hand.0.config.x_offset) * 0.6) / window.width()
-                    );
-            
-                    // move the image
-                    hand.1.translation.x = x;
-                    hand.1.translation.y = window.height() - y;
-                } else {
-                    
-                }
-            },
-            None =>{}
-        }
-    }
-
-}
-
-fn set_window_icon(
-    windows: NonSend<WinitWindows>,
-) {
+fn set_window_icon(windows: NonSend<WinitWindows>) {
     let primary = windows.get_window(WindowId::primary()).unwrap();
 
     let (icon_rgba, icon_width, icon_height) = {
@@ -179,4 +103,103 @@ fn set_window_icon(
     let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
 
     primary.set_window_icon(Some(icon));
+}
+
+fn load_image(mut commands: Commands, asset_server: Res<AssetServer>, config: Res<Config>) {
+    let texture: Handle<Image> = asset_server.load("Hand.png");
+
+    let sprite = Sprite {
+        anchor: bevy::sprite::Anchor::TopLeft,
+        ..Default::default()
+    };
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite,
+            texture,
+            ..Default::default()
+        })
+        .insert(Name::new("Hand"))
+        .insert(Transform::from_xyz(0., 0., 0.))
+        .insert(Hand {
+            config: Config {
+                x_offset: config.x_offset,
+                y_offset: config.y_offset,
+                size: config.size,
+                ..Default::default()
+            },
+        });
+}
+
+fn image_size(mut hand_query: Query<(&Hand, &mut Sprite)>) {
+    if let Ok(mut hand) = hand_query.get_single_mut() {
+        if hand.0.config.size.x > 0. && hand.0.config.size.y > 0. {
+            hand.1.custom_size = Some(hand.0.config.size);
+        }
+    } else {
+    }
+}
+
+fn setup_events(mut commands: Commands) {
+    let (sender, reciever) = unbounded::<rdev::Event>();
+    thread::spawn(move || {
+        listen(move |event| sender.send(event).expect("Failed to send event"))
+            .expect("Could not listen");
+    });
+    commands.insert_resource(StreamReceiver(reciever));
+}
+
+fn read_stream(receiver: ResMut<StreamReceiver>, mut events: EventWriter<StreamEvent>) {
+    for from_stream in receiver.try_iter() {
+        events.send(StreamEvent(from_stream));
+    }
+}
+
+fn image_movement(
+    windows: Res<Windows>,
+    mut hand_query: Query<(&Hand, &mut Transform)>,
+    mut reader: EventReader<StreamEvent>,
+) {
+    for event in reader.iter() {
+        match event.0.event_type {
+            rdev::EventType::MouseMove { x: mouse_x, y: mouse_y } => {
+                // get the window resource from bevy
+
+                let window = windows.get_primary();
+
+                // get the mouse location in a tuple
+
+                // get the image
+
+                if let Ok((hand, mut hand_transform)) = hand_query.get_single_mut() {
+                    match window {
+                        Some(window) => {
+                            // check if available
+                            // then get the position of the window
+                            if let Some(position) = window.position() {
+                                // subtract the position of the window from the mouse location
+                                // to get the relative location
+
+                                let x = (mouse_x as f32 - position.x as f32) + hand.config.x_offset; // the more offset the more the images goes to the right
+                                let y = (mouse_y as f32 - position.y as f32) - hand.config.y_offset; // the more offset the more the images goes up
+
+                                // rotating the image to add hand like effect
+                                hand_transform.rotation = Quat::from_rotation_z(
+                                    ((y * 0.8) / window.height()) - (((mouse_x as f32 - position.x as f32) - hand.config.x_offset) * 0.6) / window.width()
+                                );
+
+                                // move the image
+                                hand_transform.translation.x = x;
+                                hand_transform.translation.y = window.height() - y;
+                            } else {
+
+                            }
+                        },
+                        None =>{}
+                    }
+                }
+            }
+            _=> {}
+        }
+    }
 }
